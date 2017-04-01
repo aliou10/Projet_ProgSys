01178a3e06b2a8e4a2fc76e43358ed6dc8d98c9e
mamour <coolertall@gmail.com>
2017-04-01 16:45:54 +0000

    Chiffrement RSA 1.1 : Suppression,pour chaque processus Pi,de son messagee et envoie du reste au processus Pi-1 lors de la communication en sens inverse.

--- Projet_ProgSys.c
+++ Projet_ProgSys.c
@@ -24,6 +24,7 @@ int main(int argc, char **argv) {
     int desc1, desc2;
     int desc3, desc4;
     char messageAller[100] = "processus1";
+    char messageRetour[100];
     int numeroProc = 0;
 
 
@@ -90,22 +91,27 @@ int main(int argc, char **argv) {
             write(desc2, messageAller, sizeof(messageAller));
             printf("Processus[%d] : valeur envoyée %s.\n", getpid(), messageAller);
             read(desc1, messageAller, sizeof(messageAller));
-            sleep(3);
-            printf("Processus[%d] : valeur lue %s.\n", getpid(), messageAller);
+            //sleep(3);
+            printf("Processus[%d] : valeur lue %s.\n\n\n", getpid(), messageAller);
             close(desc2);
             close(desc1);
+            //Fin Parcours premier sens
 
-            sleep((unsigned int) numeroProc);
+            //Debut parcours sens inverse
+            sleep(4);
+            strcpy(messageRetour, messageAller);
             desc4 = open(backwardPipe[0], O_WRONLY);
             desc3 = open(backwardPipe[nombreDeProcessus - 1], O_RDONLY);
-            write(desc4, messageAller, sizeof(messageAller));
-            printf("Processus[%d] : valeur envoyée %s.\n", getpid(), messageAller);
-            read(desc3, messageAller, sizeof(messageAller));
-            printf("Processus[%d] : valeur lue %s.\n", getpid(), messageAller);
+            write(desc4, messageRetour, sizeof(messageRetour));
+            //printf("Processus[%d] : valeur envoyée %s.\n", getpid(), messageRetour);
+            read(desc3, messageRetour, sizeof(messageRetour));
+            printf("Processus[%d] : valeur lue %s.\n", getpid(), messageRetour);
             close(desc4);
             close(desc3);
             fflush(stdout);
+            //Fin parcours sens inverse pour P1
         } else {
+            //Debut parcours premier sens
             desc1 = open(forwardPipe[numeroProc - 2], O_RDONLY);
             desc2 = open(forwardPipe[numeroProc - 1], O_WRONLY);
             read(desc1, messageAller, sizeof(messageAller));
@@ -115,13 +121,13 @@ int main(int argc, char **argv) {
             sprintf(str, "%d", numeroProc);
             strcat(messageAller, str);
             write(desc2, messageAller, sizeof(messageAller));
+
             /**
              * Le processus Pn ecrit dans le fichier
              * sharedInfo.txt les messages partages
              * par les processus
              */
             if (numeroProc == nombreDeProcessus) {
-                char buf[100];
                 int fd;
                 fd = open("sharedInfo.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
                 write(fd, messageAller, strlen(messageAller) * sizeof(char));
@@ -130,17 +136,31 @@ int main(int argc, char **argv) {
             printf("Processus[%d] : valeur envoyée %s.\n", getpid(), messageAller);
             close(desc1);
             close(desc2);
+            //Fin parcours premier sens
 
-            sleep((unsigned int) numeroProc);
+            //Debut parcours sens inverse
+            sleep(4);
             desc3 = open(backwardPipe[nombreDeProcessus - numeroProc], O_RDONLY);
             desc4 = open(backwardPipe[nombreDeProcessus - numeroProc + 1], O_WRONLY);
-            read(desc3, messageAller, sizeof(messageAller));
-            printf("Processus[%d] : valeur lue %s.\n", getpid(), messageAller);
+            read(desc3, messageRetour, sizeof(messageRetour));
+            printf("Processus[%d] : valeur lue %s.\n", getpid(), messageRetour);
 
-            write(desc4, messageAller, sizeof(messageAller));
-            printf("Processus[%d] : valeur envoyée %s.\n", getpid(), messageAller);
+            /**
+             * Chaque processus Pi enleve son
+             * information et envoie le reste
+             * a Pi-1
+             */
+            char buf[100];
+            for (int i = 0; i < strlen(messageRetour) - 10; i++) {
+                buf[i] = messageRetour[i];
+            }
+            strcpy(messageRetour, buf);
+
+            write(desc4, messageRetour, sizeof(messageRetour));
+            printf("Processus[%d] : valeur envoyée %s.\n", getpid(), messageRetour);
             close(desc3);
             close(desc4);
+            //fin parcours sens inverse
         }
     }
 
